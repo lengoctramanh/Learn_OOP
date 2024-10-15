de36f59d8638a38deef148af0b83777c2c03ec99
Le Ngoc Tram Anh <letramanh2182005@gmail.com>
2024-10-15 23:36:28 +0700

    Learn_OOP_Day3

---
+++ encapsulation.js
@@ -0,0 +1,56 @@
+// Encapsulation (Đóng gói)
+//  * + bảo vệ trạng thái bên trong của đối tượng 
+//  * +phép truy cập và sửa đổi các thuộc tính thông qua các phương thức (getter và setter).
+//  * +ngăn chặn các thay đổi trực tiếp đến thuộc tính từ bên ngoài, 
+class Person {
+    // #:Thuộc tính private
+    #name
+    #age
+    //Tạo thuộc tính
+    constructor(name, age,gender, phoneNumber){
+        // class có thuộc tính này,cho phép truy cập vào phương thức
+        // thuộc tính của đối tượng này
+        this.#name=name;
+         this.#age=age ;
+         this.gender=gender;
+          this.phoneNumber=phoneNumber;
+    }
+    // Method chọt thẳng vào property name
+    getName(){
+        return this.#name
+    }
+   // Method thay đổi property name
+   setName(newName){
+       this.#name=newName
+   }
+   
+   // Method chọt thẳng vào property age
+    getAge(){
+        return this.#age
+    }
+   // Method thay đổi property age
+   setAge(newAge){
+     if(newAge>0){
+         this.#age=newAge
+     }
+     else{
+         console.log("Vui long nhap lai")
+     }
+   }
+   
+}
+const person_child=new Person("ểgth",32)
+   
+   console.log(person_child.getName())
+   person_child.setName("adrgthh")
+   console.log(person_child.getName())
+   
+   console.log(person_child.getAge())
+   person_child.setAge(2436)
+   console.log(person_child.getAge())
+   
+   person_child.setAge(-5)
+   
+
+   //BỊ LỖI ĐANG SỬA
+   
